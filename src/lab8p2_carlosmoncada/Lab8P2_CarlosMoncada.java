9a71ac92385c92ca75bc676dd3e8530a2e550f42
cabeto123 <carlberto15@gmail.com>
2023-09-08 13:23:53 -0600

    Creacion

---
+++ src/lab8p2_carlosmoncada/Lab8P2_CarlosMoncada.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab8p2_carlosmoncada;
+
+/**
+ *
+ * @author calol
+ */
+public class Lab8P2_CarlosMoncada {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
